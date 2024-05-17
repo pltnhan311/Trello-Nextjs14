da423452b58a7d5de230be035a795dd6f6e869b1
pltnhan311 <92429264+pltnhan311@users.noreply.github.com>
2024-05-17 20:02:03 +0700

    initial project

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from '@/components/mode-toggle'
+import Link from 'next/link'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <ModeToggle />
+    </div>
+  )
+}
